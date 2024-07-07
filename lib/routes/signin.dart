b12b28aa494db71110892afbeafbe1ef7f4b381c
javarava <114555092+javarava@users.noreply.github.com>
2024-07-07 04:59:32 -0400

    modified:   lib/routes/signin.dart

--- lib/routes/signin.dart
+++ lib/routes/signin.dart
@@ -10,6 +10,29 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              const SliverAppBar(
+                //title: const Text('Inventory'),
+                expandedHeight: 46,
+                toolbarHeight: 46,
+                floating: true,
+                snap: true,
+              ),
+            ];
+          },
+          body: const Padding(
+            padding: const EdgeInsets.all(0),
+            child: Column(
+              children: [SizedBox(height: 40, child: Text('Sign in'))],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
