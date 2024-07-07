c62baca064cdd8c67815091b670b18f2321f147b
Tunde Aniyi <114555092+javarava@users.noreply.github.com>
2024-07-07 10:53:21 +0100

    new file:   assets/icons/account-lock-icon.png 	new file:   assets/icons/apple.png 	new file:   assets/icons/google.png 	new file:   assets/icons/identity-icon.png 	new file:   assets/icons/mail-icon.png 	new file:   assets/icons/password-eye.png 	deleted:    lib/routes/register.dart 	new file:   lib/routes/signup.dart 	modified:   lib/src/navigation.dart

--- lib/src/navigation.dart
+++ lib/src/navigation.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import '/providers/user_provider.dart';
 import '/main.dart';
 import '/routes/welcome.dart';
-import '/routes/register.dart';
+import '../routes/signup.dart';
 import '/routes/signin.dart';
 import '/routes/error.dart';
 import '/routes/home.dart';
@@ -244,12 +244,12 @@ final goRouter = GoRouter(
           ),
         ),
         GoRoute(
-          path: 'register',
+          path: 'signup',
           pageBuilder: (context, state) => slideDownToUpTransition(
             context: context,
             state: state,
             key: UniqueKey(),
-            child: const Register(),
+            child: const SignUp(),
           ),
         ),
       ],
@@ -270,18 +270,18 @@ final goRouter = GoRouter(
 
       //Initialize anonymous user routes
       final signin = state.fullPath == '/welcome/signin';
-      final register = state.fullPath == '/welcome/register';
+      final signup = state.fullPath == '/welcome/signup';
       final welcome = state.fullPath == '/welcome';
 
       //Check if user is logged in
       if (loggedIn != null) {
-        if (signin || register || welcome) {
+        if (signin || signup || welcome) {
           return '/';
         } else {
           return null;
         }
       } else {
-        if (signin || register || welcome) {
+        if (signin || signup || welcome) {
           return null;
         } else {
           return '/welcome';
