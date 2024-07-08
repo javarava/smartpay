3a6eb63d3fa22ca8e172bb8210adf30699161e69
javarava <114555092+javarava@users.noreply.github.com>
2024-07-08 23:01:46 +0100

    modified:   lib/routes/signuppin.dart

--- lib/routes/signuppin.dart
+++ lib/routes/signuppin.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/gestures.dart';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pinput/pinput.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:provider/provider.dart';
 import '/providers/user_provider.dart';
 import '/src/widgets.dart';
